0c2e506b8231cc1115a045e4a4c5d8f00ea4021b
cjjcastro <cjj.castro3@gmail.com>
2018-04-07 21:06:49 -0300

    Update count sort

--- sort.py
+++ sort.py
@@ -10,11 +10,11 @@ class sort:
         for i in range(max_value+1):
             count[i] += count[i-1]
 
-        for i in range(len(nums)):
+        for i in range(len(nums)-1, -1, -1):
             output[count[nums[i]]-1] = nums[i]
             count[nums[i]] -= 1
 
         return output
 
-x = [2, 1, 3, 4, 1, 5,2]
+x = [3, 6, 4, 1, 3, 4, 1, 4]
 print(sort.count_sort(x, max(x)+1))
